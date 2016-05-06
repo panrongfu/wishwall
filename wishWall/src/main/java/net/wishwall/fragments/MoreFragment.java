51e7eee893c30cd867d6bf7ab686d4efa900751e
pan <pan@ipushan.com>
2016-05-06 19:37:27 +0800

    增加了版本更新功能

--- wishWall/src/main/java/net/wishwall/fragments/MoreFragment.java
+++ wishWall/src/main/java/net/wishwall/fragments/MoreFragment.java
@@ -1,16 +1,29 @@
 package net.wishwall.fragments;
 
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.RemoteViews;
 import android.widget.TextView;
 
+import com.liulishuo.filedownloader.BaseDownloadTask;
+import com.liulishuo.filedownloader.FileDownloadListener;
+import com.liulishuo.filedownloader.FileDownloader;
+
+import net.wishwall.App;
 import net.wishwall.Constants;
 import net.wishwall.R;
 import net.wishwall.activities.AboutWishwall;
@@ -19,7 +32,11 @@ import net.wishwall.activities.FeedBackActivity;
 import net.wishwall.activities.MainActivity;
 import net.wishwall.activities.PersonDetail;
 import net.wishwall.utils.SpUtil;
+import net.wishwall.views.CustomDialogFragment;
 import net.wishwall.views.CustomExitDialog;
+import net.wishwall.views.CustomToast;
+
+import java.io.File;
 
 
 /**
@@ -89,7 +106,21 @@ public class MoreFragment extends Fragment implements  OnClickListener{
 
 			//版本更新
 			case R.id.more_version_updates:
-
+				FragmentTransaction ft = getActivity().getFragmentManager().beginTransaction();
+				android.app.Fragment prev = getActivity().getFragmentManager().findFragmentByTag("dialog");
+				if (prev != null) {
+					ft.remove(prev);
+				}
+				ft.addToBackStack(null);
+				// Create and show the dialog.
+				CustomDialogFragment newFragment = CustomDialogFragment.newInstance("已经是最新版本了",R.layout.version_update);
+				newFragment.show(ft, "dialog");
+				newFragment.setOnUpdateVersionListener(new CustomDialogFragment.OnUpdateVersionListener() {
+					@Override
+					public void update() {
+						versionUpdate();
+					}
+				});
 				break;
 			//意见反馈
 			case R.id.more_idea_feedback:
@@ -103,7 +134,69 @@ public class MoreFragment extends Fragment implements  OnClickListener{
 				break;
 		}
 	}
-	
+
+	/**
+	 * 版本更新
+	 */
+	private void versionUpdate() {
+
+         final NotificationManager manager = (NotificationManager)getActivity().getSystemService(Activity.NOTIFICATION_SERVICE);
+         final Notification notif =new Notification();
+         notif.icon = R.mipmap.logo;
+         notif.tickerText = "更新";
+         notif.contentView = new RemoteViews(getActivity().getPackageName(), R.layout.update_notify_layout);
+         Intent intent = new Intent(getActivity(),MainActivity.class);
+         PendingIntent pIntent = PendingIntent.getActivity(getActivity(),0,intent,0);
+         notif.contentIntent =pIntent;
+         manager.notify(0,notif);
+
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+            String savePath =  Environment.getExternalStorageDirectory()+"/wishwall/wishwall.apk";
+            FileDownloader.getImpl().create(App.downloadUrl).setPath(savePath).setListener(new FileDownloadListener() {
+                @Override
+                protected void pending(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+
+                }
+
+                @Override
+                protected void progress(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+                    float index = soFarBytes*1.0f/totalBytes*100;
+                    notif.contentView.setTextViewText(R.id.notiy_title,index+"%");
+                    notif.contentView.setProgressBar(R.id.progressBar,soFarBytes,totalBytes,false);
+                    manager.notify(0,notif);
+                }
+
+                @Override
+                protected void completed(BaseDownloadTask task) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setDataAndType(Uri.fromFile(new File(Environment
+                                    .getExternalStorageDirectory()+"/wishwall/", "wishwall.apk")),
+                            "application/vnd.android.package-archive");
+                    startActivity(intent);
+                }
+
+                @Override
+                protected void paused(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+
+                }
+
+                @Override
+                protected void error(BaseDownloadTask task, Throwable e) {
+
+                }
+
+                @Override
+                protected void warn(BaseDownloadTask task) {
+
+                }
+            }).start();
+        }else {
+            CustomToast.showMsg(getActivity(),"SD卡不可用");
+        }
+
+	}
+
+
 	public void exit() {
 
 		exitDialog = new CustomExitDialog(getActivity(),"你确定要退出当前账号？", R.style.mystyle);
@@ -122,5 +215,4 @@ public class MoreFragment extends Fragment implements  OnClickListener{
 		});
 		exitDialog.show();				
 	}
-
 }
